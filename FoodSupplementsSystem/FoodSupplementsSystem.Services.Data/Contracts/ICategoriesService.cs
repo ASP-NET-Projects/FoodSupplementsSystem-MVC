9afd8f92acffaa498d413c39a2bd7a95408a54ca
Iliana Vachkova <an_vachkova@yahoo.com>
2017-03-18 22:25:42 +0200

    Added categories services

---
+++ FoodSupplementsSystem/FoodSupplementsSystem.Services.Data/Contracts/ICategoriesService.cs
@@ -0,0 +1,22 @@
+﻿using FoodSupplementsSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodSupplementsSystem.Services.Data.Contracts
+{
+    public interface ICategoriesService
+    {
+        IQueryable<Category> GetAll();
+
+        Category Create(string name);
+
+        void UpdateNameById(int id, string name);
+
+        void DeleteById(int id);
+
+        Category GetById(int id);
+    }
+}
