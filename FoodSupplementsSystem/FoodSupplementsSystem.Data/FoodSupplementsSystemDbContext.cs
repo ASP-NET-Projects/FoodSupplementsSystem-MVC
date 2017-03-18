2a10d66190a745b240999baf3f22623c30e414fc
Iliana Vachkova <an_vachkova@yahoo.com>
2017-03-18 15:15:09 +0200

    Extracted ApplicationDbContext from .Web project to .Data project

---
+++ FoodSupplementsSystem/FoodSupplementsSystem.Data/FoodSupplementsSystemDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FoodSupplementsSystem.Data.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace FoodSupplementsSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
