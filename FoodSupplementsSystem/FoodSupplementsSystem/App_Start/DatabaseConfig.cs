8065f3ea611db27b22c40b9ba83a30789afc2c3e
Iliana Vachkova <an_vachkova@yahoo.com>
2017-03-19 18:20:39 +0200

    Added DatabaseConfig.cs

---
+++ FoodSupplementsSystem/FoodSupplementsSystem/App_Start/DatabaseConfig.cs
@@ -0,0 +1,15 @@
+﻿using FoodSupplementsSystem.Data;
+using FoodSupplementsSystem.Data.Migrations;
+using System.Configuration;
+using System.Data.Entity;
+
+namespace FoodSupplementsSystem.App_Start
+{
+    public static class DatabaseConfig
+    {
+        public static void RegisterDatabase()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<FoodSupplementsSystemDbContext, Data.Migrations.Configuration>());
+        }
+    }
+}
