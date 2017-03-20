8dcd3dab194c52366e59dfff611f392fb72f01b7
Iliana Vachkova <an_vachkova@yahoo.com>
2017-03-20 14:51:55 +0200

    Added database connection between ApplicationUser and Comment, and Supplement models

--- FoodSupplementsSystem/FoodSupplementsSystem.Data.Models/ApplicationUser.cs
+++ FoodSupplementsSystem/FoodSupplementsSystem.Data.Models/ApplicationUser.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -7,6 +8,27 @@ namespace FoodSupplementsSystem.Data.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        private ICollection<Supplement> supplements;
+        private ICollection<Comment> comments;
+
+        public ApplicationUser()
+        {
+            this.supplements = new HashSet<Supplement>();
+            this.comments = new HashSet<Comment>();
+        }
+
+        public virtual ICollection<Supplement> Supplements
+        {
+            get { return this.supplements; }
+            set { this.supplements = value; }
+        }
+
+        public virtual ICollection<Comment> Comments
+        {
+            get { return this.comments; }
+            set { this.comments = value; }
+        }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
