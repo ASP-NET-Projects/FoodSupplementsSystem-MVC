5e8657febc9a20ea08d361895311cc7bc4605541
Iliana Vachkova <an_vachkova@yahoo.com>
2017-03-23 14:55:47 +0200

    Changed database Category model with CategoryViewModel(dto) in Category View

---
+++ FoodSupplementsSystem/FoodSupplementsSystem/Areas/Administration/Models/CategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using FoodSupplementsSystem.Data.Models;
+using FoodSupplementsSystem.Infrastructure.Mapping;
+
+namespace FoodSupplementsSystem.Areas.Administration.Models
+{
+    public class CategoryViewModel : IMapFrom<Category>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
