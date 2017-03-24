a582ef03e77690da2e4033d21804434c065c0fff
Iliana Vachkova <an_vachkova@yahoo.com>
2017-03-24 12:57:34 +0200

    Added listing AllBrands for authorized users

---
+++ FoodSupplementsSystem/FoodSupplementsSystem/Controllers/AllBrandsController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper.QueryableExtensions;
+using FoodSupplementsSystem.Data.Models;
+using FoodSupplementsSystem.Data.Repositories;
+using FoodSupplementsSystem.Models.AllBrands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodSupplementsSystem.Controllers
+{
+    [Authorize]
+    public class AllBrandsController : Controller
+    {
+        private IRepository<Brand> brandsWrapper;
+
+        public AllBrandsController(IRepository<Brand> brandsWrapper)
+        {
+            this.brandsWrapper = brandsWrapper;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            IQueryable<Brand> brands = brandsWrapper.All();
+
+            var resultBrands = brands.ProjectTo<BrandViewModel>().ToList();
+
+            return View(resultBrands);
+        }
+    }
+}
---
+++ FoodSupplementsSystem/FoodSupplementsSystem/Models/AllBrands/BrandViewModel.cs
@@ -0,0 +1,18 @@
+﻿using FoodSupplementsSystem.Data.Models;
+using FoodSupplementsSystem.Infrastructure.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodSupplementsSystem.Models.AllBrands
+{
+    public class BrandViewModel : IMapFrom<Brand>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string WebSite { get; set; }
+    }
+}
