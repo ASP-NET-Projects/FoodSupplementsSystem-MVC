645f4f547148e9556ec3149b5ae53b5405acdf9c
Iliana Vachkova <an_vachkova@yahoo.com>
2017-03-25 21:45:38 +0200

    Updated AllCategoriesController and added corresponding unit tests

---
+++ FoodSupplementsSystem/FoodSupplementsSystem.Tests/FoodSupplementsSystem/Controllers/AllCategoriesControllerTests/Constructor_Should.cs
@@ -0,0 +1,32 @@
+﻿using FoodSupplementsSystem.Controllers;
+using FoodSupplementsSystem.Services.Data.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace FoodSupplementsSystem.Tests.FoodSupplementsSystem.Controllers.AllCategoriesControllerTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ReturnsAnInstance_WhenParameterIsNotNull()
+        {
+            // Arrange
+            var categoriesServiceMock = new Mock<ICategoriesService>();
+
+            // Act
+            AllCategoriesController categoriesController = new AllCategoriesController(categoriesServiceMock.Object);
+
+            // Assert
+            Assert.IsInstanceOf<AllCategoriesController>(categoriesController);
+        }
+
+        [Test]
+        public void ThrowException_WhenParametersAreNull()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new AllCategoriesController(null));
+        }
+    }
+}
--- FoodSupplementsSystem/FoodSupplementsSystem/Controllers/AllCategoriesController.cs
+++ FoodSupplementsSystem/FoodSupplementsSystem/Controllers/AllCategoriesController.cs
@@ -1,12 +1,8 @@
 ﻿using AutoMapper.QueryableExtensions;
-using FoodSupplementsSystem.Data.Models;
-using FoodSupplementsSystem.Data.Repositories;
+using Bytes2you.Validation;
 using FoodSupplementsSystem.Models.AllCategories;
 using FoodSupplementsSystem.Services.Data.Contracts;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace FoodSupplementsSystem.Controllers
@@ -14,22 +10,19 @@ namespace FoodSupplementsSystem.Controllers
     [Authorize]
     public class AllCategoriesController : Controller
     {
-        private IRepository<Category> categoriesWrapper;
-
         private ICategoriesService categories;
 
-        public AllCategoriesController(ICategoriesService categories, IRepository<Category> categoriesWrapper)
+        public AllCategoriesController(ICategoriesService categories)
         {
+            Guard.WhenArgument(categories, "categories").IsNull().Throw();
+
             this.categories = categories;
-            this.categoriesWrapper = categoriesWrapper;
         }
 
         [HttpGet]
         public ActionResult Index()
         {
-            IQueryable<Category> categories = categoriesWrapper.All();
-
-            var resultCategories = categories.ProjectTo<CategoryViewModel>().ToList();
+            var resultCategories = categories.GetAll().ProjectTo<CategoryViewModel>().ToList();
 
             return View(resultCategories);
         }
