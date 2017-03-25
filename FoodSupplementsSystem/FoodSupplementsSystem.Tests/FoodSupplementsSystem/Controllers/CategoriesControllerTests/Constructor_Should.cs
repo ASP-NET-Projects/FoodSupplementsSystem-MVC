d7260cbb97d8b08ff0039a9d72295747db465153
Iliana Vachkova <an_vachkova@yahoo.com>
2017-03-25 08:40:00 +0200

    test push

--- FoodSupplementsSystem/FoodSupplementsSystem.Tests/FoodSupplementsSystem/Controllers/CategoriesControllerTests/Constructor_Should.cs
+++ FoodSupplementsSystem/FoodSupplementsSystem.Tests/FoodSupplementsSystem/Controllers/CategoriesControllerTests/Constructor_Should.cs
@@ -5,10 +5,6 @@ using FoodSupplementsSystem.Services.Data.Contracts;
 using Moq;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FoodSupplementsSystem.Tests.FoodSupplementsSystem.Controllers.CategoriesControllerTests
 {
