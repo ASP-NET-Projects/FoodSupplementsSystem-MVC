202d0351b1cdbabb947a41708706603c6c749de1
Iliana Vachkova <an_vachkova@yahoo.com>
2017-03-26 23:54:59 +0300

    Added some unit tests for CategoriesController

---
+++ FoodSupplementsSystem/FoodSupplementsSystem.Tests/FoodSupplementsSystem/Controllers/CategoriesControllerTests/Categories_Read_Should.cs
@@ -0,0 +1,59 @@
+﻿using FoodSupplementsSystem.App_Start;
+using FoodSupplementsSystem.Areas.Administration.Controllers;
+using FoodSupplementsSystem.Areas.Administration.Models;
+using FoodSupplementsSystem.Data.Models;
+using FoodSupplementsSystem.Services.Data.Contracts;
+using Kendo.Mvc.UI;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace FoodSupplementsSystem.Tests.FoodSupplementsSystem.Controllers.CategoriesControllerTests
+{
+    [TestFixture]
+    public class Categories_Read_Should
+    {
+        [Test]
+        public void ReturnsAnInstance_WhenParameterIsNotNull_test()
+        {
+            //Arrange
+            var categoriesServiceMock = new Mock<ICategoriesService>();
+            var categories = GetCategories();
+            categoriesServiceMock.Setup(x => x.GetAll())
+                .Returns(categories.AsQueryable());
+            AutoMapperConfig.Config();
+            var controller = new CategoriesController(categoriesServiceMock.Object);
+            var kendoDataRequest = new DataSourceRequest();
+
+            //Act
+            var controllerResult = controller.Categories_Read(kendoDataRequest);
+            var jsonResult = controllerResult as JsonResult;
+            dynamic kendoResultData = jsonResult.Data;
+            var results = kendoResultData.Data as List<CategoryViewModel>;
+
+            //Assert
+            Assert.IsInstanceOf<List<CategoryViewModel>>(results);
+        }
+
+        private IEnumerable<Category> GetCategories()
+        {
+            List<Category> categories = new List<Category>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                categories.Add(new Category()
+                {
+                    Id = i,
+                    Name = "category" + i
+                });
+            }
+
+            return categories;
+        }
+    }
+}
---
+++ FoodSupplementsSystem/FoodSupplementsSystem.Tests/FoodSupplementsSystem/Controllers/CategoriesControllerTests/Index_Should.cs
@@ -0,0 +1,24 @@
+﻿using FoodSupplementsSystem.Areas.Administration.Controllers;
+using FoodSupplementsSystem.Services.Data.Contracts;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+
+namespace FoodSupplementsSystem.Tests.FoodSupplementsSystem.Controllers.CategoriesControllerTests
+{
+    [TestFixture]
+    public class Index_Should
+    {
+        [Test]
+        public void ReturnDefaultView_WhenGetToIndex()
+        {
+            // Arrange
+            var categoriesServiceMock = new Mock<ICategoriesService>();
+
+            var categoriesController = new CategoriesController(categoriesServiceMock.Object);
+
+            // Act && Assert
+            categoriesController.WithCallTo(c => c.Index()).ShouldRenderDefaultView();
+        }
+    }
+}
