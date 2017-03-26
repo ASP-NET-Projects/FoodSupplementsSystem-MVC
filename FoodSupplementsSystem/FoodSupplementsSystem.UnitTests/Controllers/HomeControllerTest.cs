51f173b1ed3bee6912da92b6cd8663ecc86dc849
Iliana Vachkova <an_vachkova@yahoo.com>
2017-03-26 16:36:50 +0300

    test

--- FoodSupplementsSystem/FoodSupplementsSystem.UnitTests/Controllers/HomeControllerTest.cs
+++ FoodSupplementsSystem/FoodSupplementsSystem.UnitTests/Controllers/HomeControllerTest.cs
@@ -6,49 +6,60 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FoodSupplementsSystem;
 using FoodSupplementsSystem.Controllers;
+using FoodSupplementsSystem.Services.Data.Contracts;
+using Moq;
 
 namespace FoodSupplementsSystem.UnitTests.Controllers
 {
     [TestClass]
     public class HomeControllerTest
     {
-       // [TestMethod]
-       // public void Index()
-       // {
-       //     // Arrange
-       //     HomeController controller = new HomeController();
-       //
-       //     // Act
-       //     ViewResult result = controller.Index() as ViewResult;
-       //
-       //     // Assert
-       //     Assert.IsNotNull(result);
-       // }
+       [TestMethod]
+       public void Index()
+       {
+            // Arrange
+            var categoriesServiceMock = new Mock<ICategoriesService>();
+            var brandsServiceMock = new Mock<IBrandsService>();
+            var supplementsServiceMock = new Mock<ISupplementsService>();
+            HomeController controller = new HomeController(categoriesServiceMock.Object, brandsServiceMock.Object, supplementsServiceMock.Object);
+       
+           // Act
+           ViewResult result = controller.Index() as ViewResult;
+       
+           // Assert
+           Assert.IsNotNull(result);
+       }
 
-       // [TestMethod]
-       // public void About()
-       // {
-       //     // Arrange
-       //     HomeController controller = new HomeController();
-       //
-       //     // Act
-       //     ViewResult result = controller.About() as ViewResult;
-       //
-       //     // Assert
-       //     Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-       // }
+       [TestMethod]
+       public void About()
+       {
+            // Arrange
+            var categoriesServiceMock = new Mock<ICategoriesService>();
+            var brandsServiceMock = new Mock<IBrandsService>();
+            var supplementsServiceMock = new Mock<ISupplementsService>();
+            HomeController controller = new HomeController(categoriesServiceMock.Object, brandsServiceMock.Object, supplementsServiceMock.Object);
+            
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+       
+           // Assert
+           Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+       }
 
-       // [TestMethod]
-       // public void Contact()
-       // {
-       //     // Arrange
-       //     HomeController controller = new HomeController();
-       //
-       //     // Act
-       //     ViewResult result = controller.Contact() as ViewResult;
-       //
-       //     // Assert
-       //     Assert.IsNotNull(result);
-       // }
+       [TestMethod]
+       public void Contact()
+       {
+            // Arrange
+            var categoriesServiceMock = new Mock<ICategoriesService>();
+            var brandsServiceMock = new Mock<IBrandsService>();
+            var supplementsServiceMock = new Mock<ISupplementsService>();
+            HomeController controller = new HomeController(categoriesServiceMock.Object, brandsServiceMock.Object, supplementsServiceMock.Object);
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+       
+           // Assert
+           Assert.IsNotNull(result);
+       }
     }
 }
