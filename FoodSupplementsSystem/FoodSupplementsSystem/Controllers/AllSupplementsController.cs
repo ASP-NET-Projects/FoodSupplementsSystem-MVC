5247be58e4dd2e0e174f9fb802a4f4e3552bb2fa
Iliana Vachkova <an_vachkova@yahoo.com>
2017-04-18 19:53:15 +0300

    Added Kendo ListView to visualize all supplements, still working on it

---
+++ FoodSupplementsSystem/FoodSupplementsSystem/Controllers/AllSupplementsController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper.QueryableExtensions;
+using FoodSupplementsSystem.Data.Models;
+using FoodSupplementsSystem.Data.Repositories;
+using FoodSupplementsSystem.Infrastructure.Populators;
+using FoodSupplementsSystem.Services.Data.Contracts;
+using FoodSupplementsSystem.ViewModels.AllSupplements;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System.Web.Mvc;
+using System.IO;
+using System.Linq;
+
+namespace FoodSupplementsSystem.Controllers
+{
+    public class AllSupplementsController : Controller
+    {
+        private ISupplementsService supplements;
+
+        private IDropDownListPopulator populator;
+
+        public AllSupplementsController(ISupplementsService supplements, IDropDownListPopulator populator)
+        {
+            this.supplements = supplements;
+            this.populator = populator;
+        }
+
+        public ActionResult All()
+        {
+            return View();
+        }
+
+        public ActionResult ReadSupplements([DataSourceRequest]DataSourceRequest request)
+        {
+            var resultSupplements = supplements.GetAll()
+                .ProjectTo<ListSupplementViewModel>();
+
+            return Json(resultSupplements.ToDataSourceResult(request));
+        }
+
+        public ActionResult GetCategories()
+        {
+            return Json(this.populator.GetCategories(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ FoodSupplementsSystem/FoodSupplementsSystem/ViewModels/AllSupplements/ListSupplementViewModel.cs
@@ -0,0 +1,32 @@
+﻿using FoodSupplementsSystem.Data.Models;
+using FoodSupplementsSystem.Infrastructure.Mapping;
+using AutoMapper;
+
+namespace FoodSupplementsSystem.ViewModels.AllSupplements
+{
+    public class ListSupplementViewModel : IMapFrom<Supplement>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        //public string Image { get; set; }
+
+        public string SupplementName { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string BrandName { get; set; }
+
+        public string TopicName { get; set; }
+
+        public void CreateMappings(IMapperConfigurationExpression config)
+        {
+            config.CreateMap<Supplement, ListSupplementViewModel>()
+                //.ForMember(m => m.Image, opt => opt.MapFrom(s => s.ImageUrl))
+                .ForMember(m => m.SupplementName, opt => opt.MapFrom(s => s.Name))
+                .ForMember(m => m.CategoryName, opt => opt.MapFrom(s => s.Category))
+                .ForMember(m => m.BrandName, opt => opt.MapFrom(s => s.Brand))
+                .ForMember(m => m.TopicName, opt => opt.MapFrom(s => s.Topic))
+                .ReverseMap();
+        }
+    }
+}
