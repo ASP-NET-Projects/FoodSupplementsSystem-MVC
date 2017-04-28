390a71864602c84e82216eb6108c1a08bc8cf750
Iliana Vachkova <an_vachkova@yahoo.com>
2017-04-29 01:35:18 +0300

    Added feedback form

---
+++ FoodSupplementsSystem/FoodSupplementsSystem/Controllers/AllFeedbacksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodSupplementsSystem.Controllers
+{
+    public class AllFeedbacksController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ FoodSupplementsSystem/FoodSupplementsSystem/ViewModels/AllFeedbacks/FeedbackViewModel.cs
@@ -0,0 +1,16 @@
+﻿using FoodSupplementsSystem.Infrastructure.Mapping;
+using System.ComponentModel.DataAnnotations;
+using FoodSupplementsSystem.Data.Models;
+
+namespace FoodSupplementsSystem.ViewModels.AllFeedbacks
+{
+    public class FeedbackViewModel : IMapFrom<Feedback>
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+    }
+}
