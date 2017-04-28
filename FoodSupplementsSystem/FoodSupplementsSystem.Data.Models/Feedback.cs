4f789df476e9ab9d1c88c37806ad0943c211e804
Iliana Vachkova <an_vachkova@yahoo.com>
2017-04-28 23:45:59 +0300

    Added Feedback data model

---
+++ FoodSupplementsSystem/FoodSupplementsSystem.Data.Models/Feedback.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodSupplementsSystem.Data.Models
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public string AuthorId { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public virtual ApplicationUser Author { get; set; }
+    }
+}
