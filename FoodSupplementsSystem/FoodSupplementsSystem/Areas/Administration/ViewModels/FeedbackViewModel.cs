de07fb9624a086b68a83cc4c9d8a9af29ef27872
Iliana Vachkova <an_vachkova@yahoo.com>
2017-05-01 17:52:36 +0300

    Protected PageableFeedbackList page from XSS by sanitizing the content before displaying it

--- FoodSupplementsSystem/FoodSupplementsSystem/Areas/Administration/ViewModels/FeedbackViewModel.cs
+++ FoodSupplementsSystem/FoodSupplementsSystem/Areas/Administration/ViewModels/FeedbackViewModel.cs
@@ -2,14 +2,24 @@
 using FoodSupplementsSystem.Infrastructure.Mapping;
 using System;
 using AutoMapper;
+using FoodSupplementsSystem.Infrastructure.Sanitizer;
 
 namespace FoodSupplementsSystem.Areas.Administration.ViewModels
 {
     public class FeedbackViewModel : IMapFrom<Feedback>, IHaveCustomMappings
     {
+        private ISanitizer sanitizer;
+
+        public FeedbackViewModel()
+        {
+            sanitizer = new HtmlSanitizerAdapter();
+        }
+
         public string Title { get; set; }
 
         public string Content { get; set; }
+
+        public string SanitizedContent => this.sanitizer.Sanitize(this.Content);
 
         public DateTime CreationDate { get; set; }
 
