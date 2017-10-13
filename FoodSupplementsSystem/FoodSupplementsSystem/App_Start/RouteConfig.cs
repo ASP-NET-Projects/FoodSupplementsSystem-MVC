2969a797cbcc4f0b44757d13a8b443cb4bd4ab20
Iliana Vachkova <an_vachkova@yahoo.com>
2017-10-13 16:09:39 +0300

    Lowercased urls

--- FoodSupplementsSystem/FoodSupplementsSystem/App_Start/RouteConfig.cs
+++ FoodSupplementsSystem/FoodSupplementsSystem/App_Start/RouteConfig.cs
@@ -7,6 +7,8 @@ namespace FoodSupplementsSystem
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.LowercaseUrls = true;
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
